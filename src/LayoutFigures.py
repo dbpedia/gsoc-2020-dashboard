25dc9f27e9b189f7626a1592c098b26e5e68af68
karankharecha <kharechakaran67@gmail.com>
2020-08-13 01:42:26 -0300

    log message changes

--- src/LayoutFigures.py
+++ src/LayoutFigures.py
@@ -12,7 +12,7 @@ def ontologyHierarchy():
     ontologyData = ''
     if path.exists(dataPath + '/Ontologies.csv'):
         ontologyData = pd.read_csv(dataPath + '/Ontologies.csv')
-        print('done 1')
+        print('ontologies fetched from the file')
     else:
         results = RD.sparqlWrapper(
             "select ?class ?subclass ?depth { ?subclass rdfs:subClassOf ?class . { select ?subclass (COUNT(?class)-1 AS ?depth) { ?subclass rdfs:subClassOf* ?class . ?class rdfs:subClassOf* owl:Thing . } } } ORDER BY ?depth ?class ?subclass",
@@ -27,7 +27,7 @@ def instanceCount():
     if path.exists(dataPath + '/instancesCount.csv') and path.exists(dataPath + '/parentClasses.csv'):
         parentClasses = pd.read_csv(dataPath + '/parentClasses.csv')
         instancesCount = pd.read_csv(dataPath + '/instancesCount.csv')
-        print('done 2')
+        print('instances fetched from the file')
     else:
         results = RD.sparqlWrapper(
             "SELECT distinct ?class ?subclass count (distinct ?instance) as ?tier2count  WHERE {{ ?subclass rdfs:subClassOf ?class FILTER (?class in (dbo:Person, dbo:Organisation, dbo:Place, dbo:Work, dbo:Event)) } UNION { SELECT ?subclass ?class { VALUES (?subclass ?class){ (dbo:Person dbo:Person) (dbo:Organization dbo:Organization) (dbo:Place dbo:Place) (dbo:Work dbo:Work) (dbo:Event dbo:Event)}}} ?instance rdf:type/rdfs:subClassOf* ?subclass . } Group by ?class ?subclass ORDER by ?class",
